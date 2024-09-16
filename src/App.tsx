f3a620b22af98de37e66d9e745f920733e79a83e
PrimeRin <primedruk312@gmail.com>
2024-09-16 14:47:41 +0600

    new form ui done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
+import NewNote from "./NewNote";
 function App() {
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<div>Hi</div>} />
-        <Route path="/new" element={<div>new</div>} />
+        <Route path="/new" element={<NewNote />} />
         <Route path="/:id">
           <Route index element={<div>show</div>} />
           <Route path="edit" element={<div>edit</div>} />

7562449897ec5ab94edb179cdfe5a893079f70f1
PrimeRin <primedruk312@gmail.com>
2024-09-16 10:34:14 +0600

    routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
-    <>
-     <h1>Hi</h1>
-    </>
-  )
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<div>Hi</div>} />
+        <Route path="/new" element={<div>new</div>} />
+        <Route path="/:id">
+          <Route index element={<div>show</div>} />
+          <Route path="edit" element={<div>edit</div>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
